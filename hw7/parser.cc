7701190018a392b78bf24020bf6d9f4f243994ca
Prakhar Srivastav <prakhar1989@gmail.com>
2016-11-26 21:01:03 -0500

    shortened the parsing code for vertices

--- hw7/parser.cc
+++ hw7/parser.cc
@@ -60,37 +60,15 @@ unsigned long Parser::parse_obj (
     printf("Read %lu vertices and %lu faces from '%s'\n",
            vertices.size() / 3, tris.size() / 3, file_name.c_str());
 
-    const unsigned long n_vertices = tris.size();
-
+    // build the vertices array as an array of floats
+    unsigned long n_vertices = tris.size();
     for (auto i = 0; i < n_vertices / 3; i++) {
-        int v1, v2, v3;
-        float x1, y1, z1, x2, y2, z2, x3, y3, z3;
-
-        v1 = tris[3*i];
-        v2 = tris[3*i+1];
-        v3 = tris[3*i+2];
-
-        x1 = verts[3 * v1];
-        y1 = verts[3 * v1 + 1];
-        z1 = verts[3 * v1 + 2];
-
-        x2 = verts[3 * v2];
-        y2 = verts[3 * v2 + 1];
-        z2 = verts[3 * v2 + 2];
-
-        x3 = verts[3 * v3];
-        y3 = verts[3 * v3 + 1];
-        z3 = verts[3 * v3 + 2];
-
-        vertices.push_back(x1);
-        vertices.push_back(y1);
-        vertices.push_back(z1);
-        vertices.push_back(x2);
-        vertices.push_back(y2);
-        vertices.push_back(z2);
-        vertices.push_back(x3);
-        vertices.push_back(y3);
-        vertices.push_back(z3);
+        for (auto j = 0; j < 3; j++) {
+            int vertex = tris[3 * i + j];
+            for (auto k = 0; k < 3; k++) {
+                vertices.push_back(verts[3 * vertex + k]);
+            }
+        }
     }
 
     return n_vertices;
